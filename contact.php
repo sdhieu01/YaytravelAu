fb5d32341270a0065e512859867e70906f845906
sdhieu01 <41766837+sdhieu01@users.noreply.github.com>
2018-10-29 07:34:02 +1100

    f

--- contact.php
+++ contact.php
@@ -1,37 +1,15 @@
-<?php
-$from = '';
-$sendTo = 'sdhieu01@cgstudents.catholic.edu.au';
-$subject = 'New message from contact form';
-$fields = array('name' => 'Name', 'email' => 'Email', 'subject' => 'Subject', 'message' => 'Message');
+ <?php
 
-$okMessage = 'Thank you for your message. I will write back soon.';
-$errorMessage = 'There is an error while sending message. Please try again later.';
+ if (isset($_POST['submit'])) {
+   $name = $_POST['name'];
+   $subject = $_POST['subject'];
+   $emalFrom = $_POST['mail'];
+   $message = $_POST['message'];
 
-try {if (!empty($_POST)) {
-    $emailText = "You have a new message from your contact form\n=====\n";
-    foreach ($_POST as $key => $value) {
-       if (isset($fields[$key])) {
-         $emailText .= "$fields[$key]: $value\n";
-       }
-    }
-    $headers = array('Content-Type: text/plain; charset="UTF-8";',
-     'From: ' . $from,
-     'Reply-To: ' . $from,
-     'Return-Path: ' . $from,
-    );
-    mail($sendTo, $subject, $emailText, implode("\n", $headers));
-    $responseArray = array('type' => 'success', 'message' => $okMessage);
-    }
-}
-catch (\Exception $e) {
-  $responseArray = array('type' => 'danger', 'message' => $e->getMessage());
-}
-if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
- $encoded = json_encode($responseArray);
- header('Content-Type: application/json');
- echo $encoded;
-} else {
-echo $responseArray['message'];
-}
+$mailTo ="sdhieu01@cgstudents.catholic.edu.au";
+$headers = "From:".$emailFrom;
+$txt = "You have received an e-mail from ".$name.".\n\n".$message;
 
-/*php followed by a space and end-of-line*/ echo "a"?>
+   mail($mailTo. $subject, $txt, $headers);
+   header(Location: contact.html?mailsend")
+ }
