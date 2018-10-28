1f1872f8ed86e6090a834f0b00181a10e7f32686
sdhieu01 <41766837+sdhieu01@users.noreply.github.com>
2018-10-29 08:26:07 +1100

    f

--- popup.php
+++ popup.php
@@ -1,13 +1,14 @@
 <?php
 
 if (isset($_POST['submit'])) {
-  $name = $_POST['firstname'];
-  $name = $_POST['lastname'];
+  $name = $_POST['name'];
   $subject = $_POST['subject'];
+  $emalFrom = $_POST['email'];
+  $message = $_POST['message'];
 
 $mailTo ="sdhieu01@cgstudents.catholic.edu.au";
-$headers = "From:".$name;
-$txt = "You have received an e-mail from ".$name.".\n\n".$subject;
+$headers = "From:".$emailFrom;
+$txt = "You have received an e-mail from ".$name.".\n\n".$message;
 
   mail($mailTo. $subject, $txt, $headers);
   header(Location: index.html?mailsend")
