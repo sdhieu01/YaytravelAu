8a33a191591c28c19c537a9448477b25f2e9569d
sdhieu01 <41766837+sdhieu01@users.noreply.github.com>
2018-10-29 12:04:39 +1100

    g

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title> Seach Bar</title>
+  <link rel="stylesheet" href="css/search.css"/>
+  <script type="text/javascript">
+
+  </script>
+</head>
+<body>
+  <form action="search.php" method="post">
+     <input type="text" id="seachBar" placeholder="Seach" value="" maxlength="25" autocomplete="off" /<
+     <intput type=submit" id="searchBtn" value="GO!" />
+   </form>
+ </body>
+ </html>
+
+
+  
