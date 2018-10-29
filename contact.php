61d6297861bcab11b9c246aa43bddf159a6817a2
sdhieu01 <41766837+sdhieu01@users.noreply.github.com>
2018-10-29 15:50:34 +1100

    f

--- contact.php
+++ contact.php
@@ -11,5 +11,4 @@ $headers = "From:".$emailFrom;
 $txt = "You have received an e-mail from ".$name.".\n\n".$message;
 
    mail($mailTo. $message, $txt, $headers);
-   header(Location: contact.html?mailsend")
  }
