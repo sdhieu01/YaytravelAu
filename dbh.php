7ca6cebd788bd1d881f5088ba0c0603765dd3bb1
sdhieu01 <41766837+sdhieu01@users.noreply.github.com>
2018-10-29 14:51:08 +1100

    F

---
+++ dbh.php
@@ -0,0 +1,8 @@
+<?php
+
+$server ="localhost";
+$username="root";
+$password="";
+$dbname = "dbphsearch";
+
+$conn = mysqli_connect($server, $username, $password, $dbname);
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+      include 'header.php';
+ ?>
+
+ <form action="search.php" method="POST">
+   <input type="text" name="search" placeholder="Search">
+   <button type="submit" naem="submit-search"></button>
+ </form>
+
+</body>
+</html>
